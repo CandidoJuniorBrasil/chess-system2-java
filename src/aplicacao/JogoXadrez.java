aa42d14e1210f7a3065f6459f70887dcfb3381bb
Candido Junior <cand2005@gmail.com>
2024-01-04 19:46:37 -0300

    Projeto inicial

---
+++ src/aplicacao/JogoXadrez.java
@@ -0,0 +1,12 @@
+package aplicacao;
+
+public class JogoXadrez {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world! com vocês");
+		System.out.println("Forte abraço");
+
+	}
+
+}
