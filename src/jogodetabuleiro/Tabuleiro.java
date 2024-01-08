5c1f05073c677b2ad1afcefac3d4e7c84a7cd3c6
Candido Junior <cand2005@gmail.com>
2024-01-08 18:33:27 -0300

    Exceções do Tabuleiro e programação defensiva

--- src/jogodetabuleiro/Tabuleiro.java
+++ src/jogodetabuleiro/Tabuleiro.java
@@ -7,6 +7,9 @@ public class Tabuleiro {
 	private Elemento[][] elementos;
 	
 	public Tabuleiro(int linha, int coluna) {
+		if (linha < 1 || coluna < 1) {
+			throw new ExcecoesTabuleiro("Erro criando Tabuleiro: deve haver pelo menos 1 linha e 1 coluna");
+		}
 		this.linha = linha;
 		this.coluna = coluna;
 		elementos = new Elemento[linha][coluna];
@@ -16,29 +19,45 @@ public class Tabuleiro {
 		return linha;
 	}
 
-	public void setLinha(int linha) {
-		this.linha = linha;
-	}
-
 	public int getColuna() {
 		return coluna;
 	}
 
-	public void setColuna(int coluna) {
-		this.coluna = coluna;
-	}
-	
 	public Elemento elemento(int linha, int coluna) {
+		if (! existePosicao(linha,coluna)) {
+			throw new ExcecoesTabuleiro("Não existe esta posição no Tabuleiro");
+		}
 		return elementos[linha][coluna];
 	}
 	
 	public Elemento elemento(Posicao posicao) {
+		if (! existePosicao(posicao)) {
+			throw new ExcecoesTabuleiro("Não existe esta posição no Tabuleiro");
+		}
 		return elementos[posicao.getLinha()][posicao.getColuna()];
 	}
 	
 	public void movimentaElemento(Elemento elemento, Posicao posicao ) {
+		if (haUmElemento(posicao)) {
+			throw new ExcecoesTabuleiro("Já existe uma peça nesta posição" + posicao);
+		}
 		elementos[posicao.getLinha()][posicao.getColuna()] = elemento;
 		elemento.posicao = posicao;
+	}
+	
+	public boolean existePosicao(int linha, int coluna ) {
+		return linha >= 0 && linha <= this.linha && coluna >= 0 && coluna <= this.coluna;
+	}
+	
+	public boolean existePosicao(Posicao posicao ) {
+		return existePosicao(posicao.getLinha(), posicao.getColuna());	
+	}
+	
+	public boolean haUmElemento(Posicao posicao) {
+		if (! existePosicao(linha,coluna)) {
+			throw new ExcecoesTabuleiro("Não existe esta posição no Tabuleiro");
+		}
+		return elemento(posicao) != null;
 	}
 
 }
